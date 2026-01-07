878765f57b310fe2c36bc58848261c43065e47f8
Andreas Vratny <andreas@vratny.de>
2026-01-07 19:12:25 +0100

    fixed auto migrate

--- docker/start.sh
+++ docker/start.sh
@@ -3,6 +3,13 @@
 echo "Starting services..."
 /usr/sbin/php-fpm8.4 -R --nodaemonize &
 nginx &
+sleep 5 &
+
+for i in 1 2 3 4 5; do
+  cd /var/www/html && php artisan migrate --force && break
+  echo "Migrate failed, retrying in 5 seconds..."
+  sleep 5
+done &
 
 for i in 1 2 3 4 5; do
   cd /var/www/html && php artisan services:start && break
