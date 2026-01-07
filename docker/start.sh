63a00faf34020a20984c2bd0b2485854d3ee6468
Andreas Vratny <andreas@vratny.de>
2026-01-07 18:42:39 +0100

    workflow

--- docker/start.sh
+++ docker/start.sh
@@ -3,6 +3,12 @@
 echo "Starting services..."
 /usr/sbin/php-fpm8.4 -R --nodaemonize &
 nginx &
-cd /var/www/html && php artisan services:start &
+
+for i in 1 2 3 4 5; do
+  cd /var/www/html && php artisan services:start && break
+  echo "Queue start failed, retrying in 5 seconds..."
+  sleep 5
+done &
+
 echo "Ready."
 tail -s 1 /var/log/nginx/*.log -f
